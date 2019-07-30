ca563b88629ac96086fe388061c919647d6793d7
suyash-purwar <purwarsuyash06@gmail.com>
2019-07-31 02:19:37 +0530

    Checked the difference between python's list and numpy's array in terms of speed

--- numpy/np1.py
+++ numpy/np1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sys
+import time
 
 array = np.array([3, 4, 1, 4, 9])
 matrix = np.array([
@@ -13,3 +14,16 @@ nplist = np.arange(0, 500)
 
 size_of_pylist = sys.getsizeof(pylist) * len(pylist)
 size_of_nplist = nplist.size * nplist.itemsize
+
+SIZE = 100000
+
+list1 = list2 = range(SIZE)
+start = time.time()
+result = [(x + y) for x, y in zip(list1, list2)]
+
+print((time.time() - start) * 1000)
+
+numpy1 = numpy2 = np.arange(SIZE)
+start = time.time()
+result = numpy1 + numpy2
+print((time.time() - start) * 1000)
