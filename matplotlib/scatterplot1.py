eafb10c6a9cbf870cf0e27e87bddef1193921b9e
suyash-purwar <purwarsuyash06@gmail.com>
2019-08-06 21:31:02 -0800

    Explored scatterplot type of chart

---
+++ matplotlib/scatterplot1.py
@@ -0,0 +1,20 @@
+from matplotlib import pyplot as plt
+
+plt.style.use('seaborn')
+
+x = [5, 7, 8, 5, 6, 7, 9, 2, 3, 4, 4, 4, 2, 6, 3, 6, 8, 6, 4, 1]
+y = [7, 4, 3, 9, 1, 3, 2, 5, 2, 4, 8, 7, 1, 6, 4, 9, 7, 7, 5, 1]
+
+colors = [7, 5, 9, 7, 5, 7, 2, 5, 3, 7, 1, 2, 8, 1, 9, 2, 5, 6, 7, 5]
+sizes = [209, 486, 381, 255, 191, 315, 185, 228, 174,
+         538, 239, 394, 399, 153, 273, 293, 436, 501, 397, 539]
+
+plt.scatter(x, y, s=sizes, c=colors, cmap='Greens', edgecolors='black', linewidths=2, alpha=1)
+
+cbar = plt.colorbar()
+cbar.set_label("Satisfaction")
+
+plt.tight_layout()
+
+plt.savefig('scatterplot1.png')
+plt.show()
